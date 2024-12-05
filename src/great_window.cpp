7540395a62d4da85bf12c68bd9ab0d18f2956583
Pavel-bur-dot <burachekpavel@gmail.com>
2024-12-05 09:50:37 +0300

    Create a new window

--- src/great_window.cpp
+++ src/great_window.cpp
@@ -20,16 +20,19 @@ Great_window::Great_window(Point xy, int w, int h, const std::string& title):Win
     [ ] (Address, Address pw) {
       reference_to<Great_window>(pw).exit();
     }
-  }
+  },
+  ts{Point {x_max()-420, y_max() - 410}, "3D-MINESWEEPER"}
 { 
   attach(level_1);
   attach(level_2);
   attach(level_3);
   attach(but_exit);
+  attach(ts);
 }
 
-void Great_window::level1(){
-  return;
+int Great_window::level1(){
+  Level1_window win_l1(Point{100, 100},700, 500, "3D-MINESWEEPER_LEVEL_1");
+  return gui_main();
 }
 void Great_window::level2(){
   return;
@@ -38,5 +41,22 @@ void Great_window::level3(){
   return;
 }
 void Great_window::exit(){
+  return;
+}
+
+Level1_window::Level1_window(Point xy, int w, int h, const std::string& title):Window{xy, w, h, title},
+  but_pause{Point {30, y_max() - 350}, 260, 60, "pause",
+    [ ] (Address, Address pw) {
+      reference_to<Level1_window>(pw).pause_func();
+    }
+  }
+
+  //ts{Point {x_max()-420, y_max() - 410}, "LEVEL 1"}
+{ 
+  attach(but_pause);
+
+}
+
+void Level1_window::pause_func(){
   return;
 }
--- src/great_window.h
+++ src/great_window.h
@@ -13,14 +13,25 @@ private:
   Graph_lib::Button level_2;
   Graph_lib::Button level_3;
   Graph_lib::Button but_exit;
+  Graph_lib::Text ts;
 
   static void cb_level1(Address, Address);
   static void cb_level2(Address, Address);
   static void cb_level3(Address, Address);
   static void cb_exit(Address, Address);
 
-  static void level1();
+  static int level1();
   static void level2();
   static void level3();
   static void exit();
+  };
+
+struct Level1_window:Window {
+  Level1_window (Point xy, int w, int h, const std::string& title);
+private:
+  Graph_lib::Button but_pause;
+
+  static void cb_pause_func(Address, Address);
+
+  static void pause_func();
   };
