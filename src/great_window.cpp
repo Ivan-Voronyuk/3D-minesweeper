26e7e072dcedded30e2d59598e93ef214eff58c6
Pavel-bur-dot <burachekpavel@gmail.com>
2024-12-08 21:07:35 +0300

    The final min version(with comments)

--- src/great_window.cpp
+++ src/great_window.cpp
@@ -1,5 +1,5 @@
 #include "great_window.h"
-#include "Graph_lib/fltk.h"
+#include <string>
 
 Great_window::Great_window(Point xy, int w, int h, const std::string& title):Window{xy, w, h, title},
   level_1{Point{x_max()-480, y_max() - 350}, 260, 60, "LEVEL 1",
@@ -17,11 +17,6 @@ Great_window::Great_window(Point xy, int w, int h, const std::string& title):Win
       reference_to<Great_window>(pw).level3();
     }
   },
-  but_exit{Point{x_max()-480, y_max() - 140}, 260, 60, "EXIT", 
-    [ ] (Address, Address pw) {
-      reference_to<Great_window>(pw).exit();
-    }
-  },
   ts{Point {x_max()-530, y_max() - 410}, "3D-MINESWEEPER"}
 {
   ts.set_font(Font::times_bold);
@@ -30,10 +25,10 @@ Great_window::Great_window(Point xy, int w, int h, const std::string& title):Win
   attach(level_1);
   attach(level_2);
   attach(level_3);
-  attach(but_exit);
   attach(ts);
 }
 
+// функции по вызову окон разных уровней
 int Great_window::level1(){
   Level1_window win_l1(Point{100, 100}, 1000, 500, "3D-MINESWEEPER_LEVEL_1");
   return gui_main();
@@ -46,80 +41,56 @@ int Great_window::level3(){
   Level3_window win_l3(Point{100, 100},1000, 500, "3D-MINESWEEPER_LEVEL_3");
   return gui_main();
 }
-void Great_window::exit(){
-  int Fl_EPS_File_Surface::close ();
-
-  //hide();
-}
-
 
 Level1_window::Level1_window(Point xy, int w, int h, const std::string& title):Window{xy, w, h, title},
-  but_quit{Point {15, y_max() - 70}, 100, 50, "EXIT",
-    [ ] (Address, Address pw) {
-      reference_to<Level1_window>(pw).quit_func1();
-    }
-  },
-  but_return{Point {15, y_max() - 140}, 100, 50, "RETURN",
-    [ ] (Address, Address pw) {
-      reference_to<Level1_window>(pw).return_func1();
-    }
-  },
-  l1txt{Point {30, 30}, "LEVEL 1"}
+  l1txt{Point {30, 30}, "LEVEL 1"},
+  rect01{Point{600, 200}, 200, 100}, // положение кубика
+  box_out1{Point{100, 100}, 30, 30, "Мин осталось:"} // поле количества оставшихся мин
+  // timeer{FL_VALUE_TIMER, 200, 100, 100, 50, "S"} было бы здорово, но у меня не получилось добавить секундомер
 { 
+  int num = 10; // переменая для хранения количества мин
+  std::string n_str = std::to_string(num);
   l1txt.set_color(Color::dark_green);
   l1txt.set_font_size(20);
   l1txt.set_font(Font::times_bold_italic);
-  attach(but_quit);
-  attach(l1txt);
-  attach(but_return);
+  attach(l1txt); 
+  attach(rect01);
+  rect01.set_fill_color(Color::green); // наверное надо будет удалить, это добавлено было просто так
+  attach(box_out1);
+  box_out1.put(n_str);
+  // Fl::add_timeout(timeer); для секундомера
 }
-
-void Level1_window::quit_func1(){
-  hide();
-}
-
-int Level1_window::return_func1(){
-  Great_window win(Point{100, 100},700, 500, "3D-MINESWEEPER");
-  return gui_main(); 
-
-}
-
 
 Level2_window::Level2_window(Point xy, int w, int h, const std::string& title):Window{xy, w, h, title},
-  but_quit{Point {15, y_max() - 70}, 100, 50, "EXIT",
-    [ ] (Address, Address pw) {
-      reference_to<Level2_window>(pw).quit_func2();
-    }
-  },
-  l2txt{Point {30, 30}, "LEVEL 2"}
+  l2txt{Point {30, 30}, "LEVEL 2"},
+  rect02{Point{600, 200}, 200, 100}, // положение кубика
+  box_out2{Point{100, 100}, 30, 30, "Мин осталось:"} // поле количества оставшихся мин
 { 
+  int num = 10; // переменая для хранения количества мин
+  std::string n_str = std::to_string(num);
   l2txt.set_color(Color::dark_yellow);
   l2txt.set_font_size(20);
   l2txt.set_font(Font::times_bold_italic);
-  attach(but_quit);
   attach(l2txt);
+  attach(rect02);
+  rect02.set_fill_color(Color::dark_yellow); // наверное надо будет удалить, это добавлено было просто так
+  attach(box_out2);
+  box_out2.put(n_str);
 }
-
-void Level2_window::quit_func2(){
-  hide();
-}
-
 
 Level3_window::Level3_window(Point xy, int w, int h, const std::string& title):Window{xy, w, h, title},
-  but_quit{Point {15, y_max() - 70}, 100, 50, "EXIT",
-    [ ] (Address, Address pw) {
-      reference_to<Level3_window>(pw).quit_func3();
-    }
-  },
-  l3txt{Point {30, 30}, "LEVEL 3"}
+  l3txt{Point {30, 30}, "LEVEL 3"},
+  rect03{Point{600, 200}, 200, 100}, // положение кубика
+  box_out3{Point{100, 100}, 30, 30, "Мин осталось:"} // поле количества оставшихся мин
 { 
+  int num = 10; // переменая для хранения количества мин
+  std::string n_str = std::to_string(num);
   l3txt.set_color(Color::red);
   l3txt.set_font_size(20);
   l3txt.set_font(Font::times_bold_italic);
-  attach(but_quit);
   attach(l3txt);
-}
-
-void Level3_window::quit_func3(){
-  hide();
+  attach(rect03);
+  rect03.set_fill_color(Color::dark_red); // наверное надо будет удалить, это добавлено было просто так
+  attach(box_out3);
+  box_out3.put(n_str);
 }
--- src/great_window.h
+++ src/great_window.h
@@ -1,7 +1,6 @@
 #include "Graph_lib/Graph.h"
 #include "Graph_lib/GUI.h"
 #include "Graph_lib/Window.h"
-#include "Graph_lib/fltk.h"
 
 #include <string>
 
@@ -13,55 +12,38 @@ private:
   Graph_lib::Button level_1;
   Graph_lib::Button level_2;
   Graph_lib::Button level_3;
-  Graph_lib::Button but_exit;
   Graph_lib::Text ts;
 
   static void cb_level1(Address, Address);
   static void cb_level2(Address, Address);
   static void cb_level3(Address, Address);
-  static void cb_exit(Address, Address);
 
   int level1();
   int level2();
   int level3();
-  void exit();
 };
-
-int create_menu();
 
 struct Level1_window:Window {
   Level1_window (Point xy, int w, int h, const std::string& title);
 private:
-  Graph_lib::Button but_quit;
   Graph_lib::Text l1txt;
-  Graph_lib::Button but_return;
-
-  static void cb_quit_func1(Address, Address);
-  static void cb_return_func1(Address, Address);
-
-  int return_func1();
-  void quit_func1();
-  int Fl_EPS_File_Surface::close ();
+  Graph_lib::Rectangle rect01; // наш кубик
+  Graph_lib::Out_box box_out1; // поле для отображения количества мин 
+  // Fl_Timer timeer; объявление секундомера
 };
 
 struct Level2_window:Window {
   Level2_window (Point xy, int w, int h, const std::string& title);
 private:
-  Graph_lib::Button but_quit;
   Graph_lib::Text l2txt;
-
-  static void cb_quit_func2(Address, Address);
-
-  void quit_func2();
+  Graph_lib::Rectangle rect02; // наш кубик
+  Graph_lib::Out_box box_out2; // поле для отображения количества мин 
 };
 
 struct Level3_window:Window {
   Level3_window (Point xy, int w, int h, const std::string& title);
 private:
-  Graph_lib::Button but_quit;
   Graph_lib::Text l3txt;
-
-  static void cb_quit_func3(Address, Address);
-
-  void quit_func3();
+  Graph_lib::Rectangle rect03; // наш кубик
+  Graph_lib::Out_box box_out3; // поле для отображения количества мин 
 };
